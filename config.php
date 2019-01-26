d57309056ea74d4a20700cd9868143bf7db22059
Programmer Rushikesh <37672619+programmerrush@users.noreply.github.com>
2019-01-26 20:37:54 +0530

    Becomes OPEN SOURCE

--- config.php
+++ config.php
@@ -17,3 +17,9 @@ session_start();
 		<a href="https://quizspot.shop/about/" class="btn "> About QuizSpot </a>
 		</div>
 	';
+
+	echo '
+		<div style="position: fixed;bottom: 11px;left: 10px;">
+		<a href="https://github.com/programmerrush/QuizSpot" class="btn "> OpenSource </a>
+		</div>
+	';
